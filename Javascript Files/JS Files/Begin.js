2b627e54fb14f9815ed1525a5b0ca8bb479034d1
MrManoj124 <manojmanorooban@gmail.com>
2025-12-03 11:42:01 +0530

    Created a HTML,CSS,JS folders

---
+++ Javascript Files/JS Files/Begin.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Beginner Course</title>
+</head>
+<body>
+    <main><h2>Hey Manorooban...!</h2></main>
+</body>
+</html>
