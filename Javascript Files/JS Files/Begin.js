b9a17e9e416a038099fdf04162268f78b8546ff3
MrManoj124 <manojmanorooban@gmail.com>
2025-12-03 12:59:58 +0530

    use length command

--- Javascript Files/JS Files/Begin.js
+++ Javascript Files/JS Files/Begin.js
@@ -1,3 +1,9 @@
-console.log("Hey Mr. Manorooban, Welcome to Javascript Beginner Course");
-console.log(typeof("This is your first Javascript code execution"));
-console.log(typeof(14));
+//console.log("Hey Mr. Manorooban, Welcome to Javascript Beginner Course");
+//console.log(typeof("This is your first Javascript code execution"));
+//console.log(typeof(14));
+
+//Strings
+myVariable="Arithmatical Logics";
+console.log(myVariable.length);
+
+
