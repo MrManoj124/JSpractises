750822924a9a414563548fc8422514ebe969c7eb
MrManoj124 <manojmanorooban@gmail.com>
2025-12-03 13:35:24 +0530

    use toFixed method

--- Javascript Files/JS Files/Begin.js
+++ Javascript Files/JS Files/Begin.js
@@ -2,39 +2,39 @@
 //console.log(typeof("This is your first Javascript code execution"));
 //console.log(typeof(14));
 
-//Strings
-myVariable="Arithmatical Logics";
-console.log(myVariable.length);
+// //Strings
+// myVariable="Arithmatical Logics";
+// console.log(myVariable.length);
 
-//String Methods
-console.log(myVariable.toUpperCase());
-console.log(myVariable.toLowerCase());
-console.log(myVariable.slice(0,5));
-console.log(myVariable.replace("Logics","Operations"));     
-console.log(myVariable.concat(" in Javascript"));
+// //String Methods
+// console.log(myVariable.toUpperCase());
+// console.log(myVariable.toLowerCase());
+// console.log(myVariable.slice(0,5));
+// console.log(myVariable.replace("Logics","Operations"));     
+// console.log(myVariable.concat(" in Javascript"));
 
-console.log(myVariable.charAt(5));
-console.log(myVariable.indexOf("L"));
-console.log(myVariable.lastIndexOf("a"));
+// console.log(myVariable.charAt(5));
+// console.log(myVariable.indexOf("L"));
+// console.log(myVariable.lastIndexOf("a"));
 
-console.log(myVariable.lastIndexOf("a"));
-console.log(myVariable.split(" "));
+// console.log(myVariable.lastIndexOf("a"));
+// console.log(myVariable.split(" "));
 
-console.log(myVariable.startsWith("Ari"));
-console.log(myVariable.endsWith("ics"));
-console.log(myVariable.endsWith("Lo"));
-console.log(myVariable.includes("mat"));
+// console.log(myVariable.startsWith("Ari"));
+// console.log(myVariable.endsWith("ics"));
+// console.log(myVariable.endsWith("Lo"));
+// console.log(myVariable.includes("mat"));
 
-myinfo="  Manorooban  ";
-console.log(myinfo.trim());   
-console.log(myinfo.repeat(3));
-console.log(myinfo.trim().repeat(2));
-console.log(myinfo.trim().length);
-console.log(myinfo.length);
-console.log(myinfo.trim().toUpperCase());
+// myinfo="  Manorooban  ";
+// console.log(myinfo.trim());   
+// console.log(myinfo.repeat(3));
+// console.log(myinfo.trim().repeat(2));
+// console.log(myinfo.trim().length);
+// console.log(myinfo.length);
+// console.log(myinfo.trim().toUpperCase());
 
-console.log(`Hello ${myinfo.trim().toUpperCase()}, Welcome to Javascript Beginner Course`);
-console.log('Hello ' + myinfo.toUpperCase() + ', Welcome to Javascript Beginner Course');
+// console.log(`Hello ${myinfo.trim().toUpperCase()}, Welcome to Javascript Beginner Course`);
+// console.log('Hello ' + myinfo.toUpperCase() + ', Welcome to Javascript Beginner Course');
 
 
 //Numbers
@@ -42,3 +42,15 @@ var num1=14;
 var num2=3.5;
 
 console.log(Number.isInteger(num1));
+console.log(Number.isInteger(num2));
+
+console.log(Number.isNaN(num1));
+console.log(Number.isNaN("Manorooban"));
+console.log(Number.isNaN(NaN));
+
+console.log(Number.parseInt(num2));
+console.log(Number.parseFloat(num2));
+console.log(num2.toFixed(1));
+console.log(num2.toFixed(3));
+console.log(num2.toPrecision(2));
+console.log(num2.toPrecision(3));
