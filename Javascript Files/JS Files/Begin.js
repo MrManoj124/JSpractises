4f0c89e4818ff72b7b1227473306acc227604f27
MrManoj124 <manojmanorooban@gmail.com>
2025-12-03 12:11:46 +0530

    use typeof command for js practice

--- Javascript Files/JS Files/Begin.js
+++ Javascript Files/JS Files/Begin.js
@@ -1,2 +1,3 @@
 console.log("Hey Mr. Manorooban, Welcome to Javascript Beginner Course");
-console.log("This is your first Javascript code execution");
+console.log(typeof("This is your first Javascript code execution"));
+console.log(typeof(14));
