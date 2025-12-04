ff32d887a7fe2f86dce4bbab1f99ea5ded49998c
MrManoj124 <manojmanorooban@gmail.com>
2025-12-04 18:54:25 +0530

    Finished practice with begin.js file

--- Javascript Files/JS Files/Begin.js
+++ Javascript Files/JS Files/Begin.js
@@ -1,40 +1,40 @@
-//console.log("Hey Mr. Manorooban, Welcome to Javascript Beginner Course");
-//console.log(typeof("This is your first Javascript code execution"));
-//console.log(typeof(14));
+console.log("Hey Mr. Manorooban, Welcome to Javascript Beginner Course");
+console.log(typeof("This is your first Javascript code execution"));
+console.log(typeof(14));
 
-// //Strings
-// const myVariable="Arithmatical Logics";
-// console.log(myVariable.length);
+//Strings
+const myVariable="Arithmatical Logics";
+console.log(myVariable.length);
 
-// //String Methods
-// console.log(myVariable.toUpperCase());
-// console.log(myVariable.toLowerCase());
-// console.log(myVariable.slice(0,5));
-// console.log(myVariable.replace("Logics","Operations"));     
-// console.log(myVariable.concat(" in Javascript"));
+//String Methods
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.slice(0,5));
+console.log(myVariable.replace("Logics","Operations"));     
+console.log(myVariable.concat(" in Javascript"));
 
-// console.log(myVariable.charAt(5));
-// console.log(myVariable.indexOf("L"));
-// console.log(myVariable.lastIndexOf("a"));
+console.log(myVariable.charAt(5));
+console.log(myVariable.indexOf("L"));
+console.log(myVariable.lastIndexOf("a"));
 
-// console.log(myVariable.lastIndexOf("a"));
-// console.log(myVariable.split(" "));
+console.log(myVariable.lastIndexOf("a"));
+console.log(myVariable.split(" "));
 
-// console.log(myVariable.startsWith("Ari"));
-// console.log(myVariable.endsWith("ics"));
-// console.log(myVariable.endsWith("Lo"));
-// console.log(myVariable.includes("mat"));
+console.log(myVariable.startsWith("Ari"));
+console.log(myVariable.endsWith("ics"));
+console.log(myVariable.endsWith("Lo"));
+console.log(myVariable.includes("mat"));
 
-// myinfo="  Manorooban  ";
-// console.log(myinfo.trim());   
-// console.log(myinfo.repeat(3));
-// console.log(myinfo.trim().repeat(2));
-// console.log(myinfo.trim().length);
-// console.log(myinfo.length);
-// console.log(myinfo.trim().toUpperCase());
+myinfo="  Manorooban  ";
+console.log(myinfo.trim());   
+console.log(myinfo.repeat(3));
+console.log(myinfo.trim().repeat(2));
+console.log(myinfo.trim().length);
+console.log(myinfo.length);
+console.log(myinfo.trim().toUpperCase());
 
-// console.log(`Hello ${myinfo.trim().toUpperCase()}, Welcome to Javascript Beginner Course`);
-// console.log('Hello ' + myinfo.toUpperCase() + ', Welcome to Javascript Beginner Course');
+console.log(`Hello ${myinfo.trim().toUpperCase()}, Welcome to Javascript Beginner Course`);
+console.log('Hello ' + myinfo.toUpperCase() + ', Welcome to Javascript Beginner Course');
 
 
 //Numbers
