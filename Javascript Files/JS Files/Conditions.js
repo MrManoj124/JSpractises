75d7846d48b16cf77737c52bb31d0ebde84d8ec3
MrManoj124 <manojmanorooban@gmail.com>
2025-12-05 17:51:35 +0530

    Use Elseif inside IfElse Statement

--- Javascript Files/JS Files/Conditions.js
+++ Javascript Files/JS Files/Conditions.js
@@ -1,9 +1,22 @@
 let todayisHeavyRain = true;
+let weather = "Heavy Rain";
+let alert = "Weather Alert: ";
 let people = "Must be aware of heavy rain";
 if (todayisHeavyRain) {
   console.log(people);
 }   
+else if (weather === "Heavy Rain") {
+    console.log(`Today ${alert} is + ${weather}`);
+    if(todayisHeavyRain){
+        console.log(people);
+    }
+    else{
+        console.log("No worry about cyclones");
+    }
+}
 else {
   console.log("No heavy rain today");
 }
 // Output: Must be aware of heavy rain
+
+
