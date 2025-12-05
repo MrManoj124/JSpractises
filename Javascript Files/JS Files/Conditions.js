955b19bf678ce75dae60b0441facf240d7c2bcbd
MrManoj124 <manojmanorooban@gmail.com>
2025-12-05 17:39:28 +0530

    Simple If-Else Condition in Js

---
+++ Javascript Files/JS Files/Conditions.js
@@ -0,0 +1,9 @@
+let todayisHeavyRain = true;
+let people = "Must be aware of heavy rain";
+if (todayisHeavyRain) {
+  console.log(people);
+}   
+else {
+  console.log("No heavy rain today");
+}
+// Output: Must be aware of heavy rain
