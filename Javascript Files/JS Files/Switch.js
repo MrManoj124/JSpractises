c83cb21fa071c0032004b7c9e70c4357d2b2967f
MrManoj124 <manojmanorooban@gmail.com>
2025-12-05 18:04:37 +0530

    Start to code  ternary Operators inside Switch.js

--- Javascript Files/JS Files/Switch.js
+++ Javascript Files/JS Files/Switch.js
@@ -28,32 +28,45 @@
 // }
 
 
-let operation = "add";
-let num1 = 10;
-let num2 = 5;
-let result;
-switch (operation) {
-    case "add":
-        result = num1 + num2;
-        console.log(`Addition: ${result}`);
-        break;
-    case "subtract":
-        result = num1 - num2;
-        console.log(`Subtraction: ${result}`);
-        break;
-    case "multiply":
-        result = num1 * num2;
-        console.log(`Multiplication: ${result}`);
-        break;
-    case "divide":
-        result = num1 / num2;
-        console.log(`Division: ${result}`);
-        break;
-        case "modulus":
-        result = num1 % num2;
-        console.log(`Modulus: ${result}`);
-        break;
-    default:
-        console.log("Invalid operation");
-        break;
-}
+// let operation = "add";
+// let num1 = 10;
+// let num2 = 5;
+// let result;
+// switch (operation) {
+//     case "add":
+//         result = num1 + num2;
+//         console.log(`Addition: ${result}`);
+//         break;
+//     case "subtract":
+//         result = num1 - num2;
+//         console.log(`Subtraction: ${result}`);
+//         break;
+//     case "multiply":
+//         result = num1 * num2;
+//         console.log(`Multiplication: ${result}`);
+//         break;
+//     case "divide":
+//         result = num1 / num2;
+//         console.log(`Division: ${result}`);
+//         break;
+//         case "modulus":
+//         result = num1 % num2;
+//         console.log(`Modulus: ${result}`);
+//         break;
+//     default:
+//         console.log("Invalid operation");
+//         break;
+// }
+
+
+//Ternary Operator Example
+let age = 20;
+let eligibility = (age >= 18) ? "Eligible to vote" : "Not eligible to vote";
+console.log(eligibility);
+// Output: Eligible to vote
+
+
+let CGPA = 3;
+let propability = (CGPA === 3) ? "Eligible for Honest Degree" : "Not eligible for Honest Degree";
+console.log(propability);
+// Output: Eligible for Honest Degree
