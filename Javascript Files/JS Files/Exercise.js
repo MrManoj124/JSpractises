fc7a1412588b40bc565b7daac55b563f120cbe7e
MrManoj124 <manojmanorooban@gmail.com>
2025-12-22 00:23:44 +0530

    Start to code Number guessing game

--- Javascript Files/JS Files/Exercise.js
+++ Javascript Files/JS Files/Exercise.js
@@ -1,13 +1,15 @@
 //let Message = alert("Hey Manorooban");
 //let Message = confirm("Ok === True\n Cancel === false");
 
-let Message = prompt("Enter Your Name: ");
-if(Message)
-{
-    console.log(Message ?? "Didn't enter the value");
-}
-else {
-    console.log("You Didn't enter your name");
-}
-console.log(Message.trim().length);
+// let Message = prompt("Enter Your Name: ");
+// if(Message)
+// {
+//     console.log(Message ?? "Didn't enter the value");
+// }
+// else {
+//     console.log("You Didn't enter your name");
+// }
+// console.log(Message.trim().length);
+
+
  
