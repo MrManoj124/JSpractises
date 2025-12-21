ec0bc0ff228849768b5a2dedb6f276e6f4598ead
MrManoj124 <manojmanorooban@gmail.com>
2025-12-22 00:31:14 +0530

    Successfully Created a number game

--- Javascript Files/JS Files/Exercise.js
+++ Javascript Files/JS Files/Exercise.js
@@ -12,4 +12,33 @@
 // console.log(Message.trim().length);
 
 
- 
+
+alert("Hey buddies Welcome to Number guessing game : ");
+
+let secret = Math.floor(Math.random() *10)+1;
+let attempts = 3;
+
+while(attempts > 0){
+    let guess = prompt("Guess a number between 1 and 10\n Attempts left :  " + attempts);
+
+
+    if(guess === null){
+        alert("Game cancelled");
+        break;
+    }
+
+    guess = Number(guess);
+
+    if(guess === secret){
+        alert("Correct! You Win!");
+        break;
+    } 
+    else{
+        alert("Wrong guess!");
+    }
+
+    attempts--;
+}
+ if(attempts === 0){
+    alert("Game over! The correct number was " + secret);
+ }
