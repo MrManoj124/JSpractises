2f4b08eb92e79da49543493ec345bc84f09d7f5c
MrManoj124 <manojmanorooban@gmail.com>
2025-12-23 16:59:26 +0530

    Update the JsGames.js file

--- Javascript Files/JS Files/JsGames.js
+++ Javascript Files/JS Files/JsGames.js
@@ -1,35 +1,60 @@
-// Rock Paper Scissor
-let playGame = confirm("Shall we Play Rock, Paper, Scissors?");
-if (playGame){
-    let playChoice = prompt("Please enter rock, paper or scissors.");
-    if(playChoice){
-    let playerOne = playerChoice.trim().toLowercase();
-    if(
-        playerOne === "rock" || playerOne === "paper" || playerOne === "scissor"
-    ){
-        let computerCoice = Math.floor(Math.random() + 3 + 1);
-        let computer = computerCoice === 1 ? "rock" : computerCoice === 2 ? "paper" : "scissors";
+// // Rock Paper Scissor
+// let playGame = confirm("Shall we Play Rock, Paper, Scissors?");
+// if (playGame){
+//     let playChoice = prompt("Please enter rock, paper or scissors.");
+//     if(playChoice){
+//     let playerOne = playerChoice.trim().toLowercase();
+//     if(
+//         playerOne === "rock" || playerOne === "paper" || playerOne === "scissor"
+//     ){
+//         let computerCoice = Math.floor(Math.random() + 3 + 1);
+//         let computer = computerCoice === 1 ? "rock" : computerCoice === 2 ? "paper" : "scissors";
     
-    let result = playerOne === computer ? "Tie Game!"
-    : playerOne === "rock" && computer === "paper"
-    ? `playerOne : ${playerOne}\n Computer : ${computer}\n Computer Wins!` : 
-    playerOne === "paper" && computer === "scissors"
-    ? `playerOne : ${playerOne}\n Computer : ${computer}\n Computer Wins!` : 
-    playerOne === "scissors" && computer === "rock"
-    ? `playerOne : ${playerOne}\n Computer : ${computer}\n Computer Wins!` : 
-     `playerOne : ${playerOne}\nComputer : ${Computer}\n playerOne Wins!`;
-        alert(result);
-        let PlayAgain = confirm("Play Again");
-        playAgain ? location.reload() : alert("Ok, Thanks for Playing .");
+//     let result = playerOne === computer ? "Tie Game!"
+//     : playerOne === "rock" && computer === "paper"
+//     ? `playerOne : ${playerOne}\n Computer : ${computer}\n Computer Wins!` : 
+//     playerOne === "paper" && computer === "scissors"
+//     ? `playerOne : ${playerOne}\n Computer : ${computer}\n Computer Wins!` : 
+//     playerOne === "scissors" && computer === "rock"
+//     ? `playerOne : ${playerOne}\n Computer : ${computer}\n Computer Wins!` : 
+//      `playerOne : ${playerOne}\nComputer : ${Computer}\n playerOne Wins!`;
+//         alert(result);
+//         let PlayAgain = confirm("Play Again");
+//         playAgain ? location.reload() : alert("Ok, Thanks for Playing .");
 
-    }else{
-        alert("You  Didn't Enter rock, paper, or Scissors.");
-    }
-}else{
-    alert("I guss you  Changed Your mind, Maybe Better luck next time.");
+//     }else{
+//         alert("You  Didn't Enter rock, paper, or Scissors.");
+//     }
+// }else{
+//     alert("I guss you  Changed Your mind, Maybe Better luck next time.");
+// }
+// }
+// else{
+//     alert("Ok, Come back Again.");
+// }
+
+
+
+//Simple Rock Paper Scissor game
+alert("You're Enter into Rock,Paper,Scissor Game !");
+
+let user = prompt("Enter rock, paper, scissors");
+let choices = ['rock', 'paper', 'scissors'];
+let computer = choices[Math.floor(Math.random() * 3)];
+
+alert("Computer choose : " + computer);
+
+if(user === computer){
+    alert("It's a tie...!");
 }
+else if(
+    (user === 'rock' && computer === 'scissors') ||
+    (user === 'paper' && computer === 'rock') ||
+    (user === 'scissors' && computer === 'paper')
+)
+{
+    alert("You Win the game..!");
 }
 else{
-    alert("Ok, Come back Again.");
+    alert("Computer win the game...!");
 }
-
