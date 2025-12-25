b2f684177ce9970b2c9b1ffd7ca8adc902995fd8
MrManoj124 <manojmanorooban@gmail.com>
2025-12-25 22:13:29 +0530

    Start for loop functions

--- Javascript Files/JS Files/Loops.js
+++ Javascript Files/JS Files/Loops.js
@@ -1,15 +1,20 @@
-//Loops 
-//#While Loop
-let num = 1;
-while(num<= 10){
-    console.log(num);
-    num=num+1;
-}
+// //Loops 
+// //#While Loop
+// let num = 1;
+// while(num<= 10){
+//     console.log(num);
+//     num=num+1;
+// }
 
-//Do While Loop
+// //Do While Loop
 
-let i=50;
-do{
+// let i=50;
+// do{
+//     console.log(i);
+//     i=i+10;
+// }
+// while(i<50)
+
+for(i=1; i<=10; i++){
     console.log(i);
-    i=i+10;
-}while(i<50)
+}
