18b708a2f968b75a77ccd15a92bdcfee1dbbf9ba
MrManoj124 <manojmanorooban@gmail.com>
2026-01-03 23:19:26 +0530

    create a first game for Ternary.js file

---
+++ Javascript Files/JS Files/Ternary.js
@@ -0,0 +1,10 @@
+// Quetion Based Ternary and Prompt function usages 
+//Single quiz game
+
+alert("Javascript Quiz");
+
+let answer = prompt("What do you think 12 + 14 ? ");
+
+let result = (Number(answer) === 26) ? "Correct Answer !" : "Wrong Answer !";
+
+alert(result);
