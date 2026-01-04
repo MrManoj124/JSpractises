76045d137c5696c6e3185466312e3753978fe7ad
MrManoj124 <manojmanorooban@gmail.com>
2026-01-05 00:13:21 +0530

    create loadQuestion for Form.js file

--- Javascript Files/JS Files/Form.js
+++ Javascript Files/JS Files/Form.js
@@ -15,3 +15,25 @@ options: ["var", "int", "string", "define"],
 answer: 0
 }
 ];
+
+let index = 0;
+let score = 0;
+
+
+function loadQuestion() {
+document.getElementById("result").innerText = "";
+
+
+let q = quiz[index];
+document.getElementById("question").innerText = q.question;
+document.getElementById("label1").innerText = q.options[0];
+document.getElementById("label2").innerText = q.options[1];
+document.getElementById("label3").innerText = q.options[2];
+document.getElementById("label4").innerText = q.options[3];
+
+
+let radios = document.getElementsByName("option");
+for (let i = 0; i < radios.length; i++) {
+radios[i].checked = false;
+}
+}
