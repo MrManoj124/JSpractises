c4bf869e5534605ef747db87867fcfea36b44dc9
MrManoj124 <manojmanorooban@gmail.com>
2026-01-05 00:12:21 +0530

    create quiz array function in Form.js file

---
+++ Javascript Files/JS Files/Form.js
@@ -0,0 +1,17 @@
+const quiz = [
+{
+question: "Which language runs in the browser?",
+options: ["Java", "C", "Python", "JavaScript"],
+answer: 3
+},
+{
+question: "What does CSS stand for?",
+options: ["Creative Style System", "Cascading Style Sheets", "Color Style Sheet", "Computer Style System"],
+answer: 1
+},
+{
+question: "Which keyword is used to declare a variable in JavaScript?",
+options: ["var", "int", "string", "define"],
+answer: 0
+}
+];
