f6d83491bcc08576e6be2e8181b84feb28c480d2
MrManoj124 <manojmanorooban@gmail.com>
2026-01-05 23:03:44 +0530

    Update the Login.html file

--- Javascript Files/JS Files/Form.js
+++ Javascript Files/JS Files/Form.js
@@ -1,19 +1,19 @@
 const quiz = [
-{
-question: "Which language runs in the browser?",
-options: ["Java", "C", "Python", "JavaScript"],
-answer: 3
-},
-{
-question: "What does CSS stand for?",
-options: ["Creative Style System", "Cascading Style Sheets", "Color Style Sheet", "Computer Style System"],
-answer: 1
-},
-{
-question: "Which keyword is used to declare a variable in JavaScript?",
-options: ["var", "int", "string", "define"],
-answer: 0
-}
+    {
+    question: "Which language runs in the browser?",
+    options: ["Java", "C", "Python", "JavaScript"],
+    answer: 3
+    },
+    {
+    question: "What does CSS stand for?",
+    options: ["Creative Style System", "Cascading Style Sheets", "Color Style Sheet", "Computer Style System"],
+    answer: 1
+    },
+    {
+    question: "Which keyword is used to declare a variable in JavaScript?",
+    options: ["var", "int", "string", "define"],
+    answer: 0
+    }
 ];
 
 let index = 0;
