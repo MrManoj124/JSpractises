3a9ec0b11be6d19674839f2c4fe99ed09f43311d
MrManoj124 <manojmanorooban@gmail.com>
2026-01-17 20:36:43 +0530

    print the string values

--- Javascript Files/JS Files/function.js
+++ Javascript Files/JS Files/function.js
@@ -16,3 +16,19 @@ function emailvalidation(email){
     return "manoj@gmail.com";
 }
 console.log(emailvalidation());
+
+console.log("Mr. "+"manoj");
+
+// function findValue(obj, searchString)
+// {
+//     for(let key in obj){
+//         if(typeof obj[key] === 'string' && obj[key].includes(searchString))
+//         {
+//             return obj[key];
+//         }
+//     }
+// }
+// let data = {email: 'krisha@gmail.com', name:'krisha'};
+// let email = findValue(data, "@");
+
+// console.log(email);
