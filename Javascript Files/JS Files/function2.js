63f946df7bab4dce6f2148b191c6712e7962c952
MrManoj124 <manojmanorooban@gmail.com>
2026-01-23 21:05:22 +0530

    code for callback function with Map

--- Javascript Files/JS Files/function2.js
+++ Javascript Files/JS Files/function2.js
@@ -6,3 +6,9 @@ console.log(toPropercase("Mr.Manorooban"));
 
 //Callback with Map
 let numbers = [1,2,3,4,5];
+let doubled = numbers.map(function(num){
+    return num * 2;
+});
+console.log("Doubled Numbers: ", doubled);
+
+
