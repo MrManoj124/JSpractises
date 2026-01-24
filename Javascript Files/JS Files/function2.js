4f4d21bbe839955d118dfeb808beb22a86387f18
MrManoj124 <manojmanorooban@gmail.com>
2026-01-24 12:17:31 +0530

    use spread operator

--- Javascript Files/JS Files/function2.js
+++ Javascript Files/JS Files/function2.js
@@ -141,4 +141,11 @@ console.log("Sum All: " + sumAll(1, 2, 3, 4, 5));
 console.log("Sum All: " + sumAll(10, 20, 30));
 
 
+//Spread Operator
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let combined = [...arr1, ...arr2];
+console.log("Combined Array: " + combined);
+
+
 
