bfa0a8672d840c3a51b9eb9f16aa78b3c9a562f5
MrManoj124 <manojmanorooban@gmail.com>
2026-01-25 14:32:21 +0530

    declare a variable using var

--- Javascript Files/JS Files/scope.js
+++ Javascript Files/JS Files/scope.js
@@ -1,1 +1,8 @@
 //Scope var, let, const
+
+// let's declare a variable using var
+var globalVar = "I am a global variable";
+var globalVar = "I can be redeclared";
+console.log(globalVar); // Output: I can be redeclared
+
+
