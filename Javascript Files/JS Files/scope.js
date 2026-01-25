5afafa38480b9b72b6f51bc2c30bf8e45d17c5cf
MrManoj124 <manojmanorooban@gmail.com>
2026-01-25 14:36:51 +0530

    demostrating block scope with let and const

--- Javascript Files/JS Files/scope.js
+++ Javascript Files/JS Files/scope.js
@@ -21,5 +21,17 @@ const constantConst = "I am a constant variable";
 console.log(constantConst); // Output: I am a constant variable
 
 
+// Demonstrating block scope with let and const
+if (true) {
+    let blockScopedLet = "I am block scoped";   
+    const blockScopedConst = "I am also block scoped";
+    console.log(blockScopedLet); // Output: I am block scoped
+    console.log(blockScopedConst); // Output: I am also block scoped
+}
+// console.log(blockScopedLet); // This will throw an error
+// console.log(blockScopedConst); // This will throw an error
+
+
+
 
 
