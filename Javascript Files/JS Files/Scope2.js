8381509eb509d7c2a53fe5e8033041998dda650f
MrManoj124 <manojmanorooban@gmail.com>
2026-01-26 23:14:45 +0530

    Start to create outer function

---
+++ Javascript Files/JS Files/Scope2.js
@@ -0,0 +1,12 @@
+//A closure is created when a function remembers variables from its outer scope,
+//even after the outer function has finished execution.
+
+function outer() {  
+    let count = 0;
+
+//     function innerFunction() {
+//         count++;
+//         console.log("Count:", count);
+//     }
+//     return innerFunction;
+// }
