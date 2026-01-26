a38b32c8419a0084f317c1567b2e526f25e54eb0
MrManoj124 <manojmanorooban@gmail.com>
2026-01-27 00:19:41 +0530

    example for declaring variable using const

--- Javascript Files/JS Files/Scope2.js
+++ Javascript Files/JS Files/Scope2.js
@@ -133,12 +133,16 @@ setupButton("myButton"); // Assuming there's a button with id "myButton" in the 
 
 // //In summary, closures are powerful tools in JavaScript that enable data privacy,
 // //function factories, state maintenance in asynchronous code, and event handling, among other use cases.
-// // Let's declare a variable using const
-// const constantConst = "I am a constant variable";   
-// // const constantConst = "I cannot be
-// //redeclared"; // This will throw an error
-// // constantConst = "I cannot be reassigned"; // This will also throw an error
-// console.log(constantConst); // Output: I am a constant variable
+
+
+//Let's declare a variable using const
+const constantConst = "I am a constant variable";   
+// const constantConst = "I cannot be
+//redeclared"; // This will throw an error
+// constantConst = "I cannot be reassigned"; // This will also throw an error
+console.log(constantConst); // Output: I am a constant variable
+
+
 // // Demonstrating block scope with let and const
 // if (true) {
 //     let blockScopedLet = "I am block scoped";   
