ed1b2fb4254067cd3688ec5d2e7fb60d926d6efc
MrManoj124 <manojmanorooban@gmail.com>
2026-01-27 23:18:22 +0530

    create Array constructors

--- Javascript Files/JS Files/Arrays.js
+++ Javascript Files/JS Files/Arrays.js
@@ -3,3 +3,7 @@
 //Creating an array
 let fruits = ["Apple", "Banana", "Cherry"];
 console.log(fruits); // Output: [ 'Apple', 'Banana', 'Cherry' ]
+
+//Array Constructors
+let nums = new Array(1, 2, 3);
+console.log(nums); // Output: [ 1, 2, 3 ]
