16349fe4addaab935615c9edb6d37daa3edd3436
MrManoj124 <manojmanorooban@gmail.com>
2026-01-27 23:19:14 +0530

    Accessing and Updating array elements

--- Javascript Files/JS Files/Arrays.js
+++ Javascript Files/JS Files/Arrays.js
@@ -7,3 +7,13 @@ console.log(fruits); // Output: [ 'Apple', 'Banana', 'Cherry' ]
 //Array Constructors
 let nums = new Array(1, 2, 3);
 console.log(nums); // Output: [ 1, 2, 3 ]
+
+
+//Accessing and Updating array elements
+let colors = ["Red", "Green", "Blue"];
+console.log(colors[0]); // Red
+
+colors[1] = "Yellow";
+console.log(colors); // ["Red", "Yellow", "Blue"]
+
+
