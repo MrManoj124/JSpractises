5826c58492e1e234ba3594d391f81f8e65fe0314
MrManoj124 <manojmanorooban@gmail.com>
2026-01-28 18:38:10 +0530

    combined two Arrays

--- Javascript Files/JS Files/Arrays.js
+++ Javascript Files/JS Files/Arrays.js
@@ -241,3 +241,5 @@ console.log(a); // 1
 //Spread Operator with Arrays
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6]; 
+let combined = [...arr1, ...arr2];
+console.log(combined); // [1, 2, 3, 4, 5, 6]
