e304a503c44e10fb07545c2db1064db2605937f9
MrManoj124 <manojmanorooban@gmail.com>
2026-01-30 22:28:39 +0530

    create linearSearch function

---
+++ Javascript Files/JS Files/Arrays2.js
@@ -0,0 +1,12 @@
+//Linear Search (Manual Algorithm)
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+// let nums = [10, 20, 30, 40];
+// console.log(linearSearch(nums, 30)); // 2
