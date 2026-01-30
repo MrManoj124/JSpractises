d2ac499b785869de2db1153ce7f384592737b49b
MrManoj124 <manojmanorooban@gmail.com>
2026-01-30 22:53:40 +0530

    create for loop for bubblesort function

--- Javascript Files/JS Files/Arrays2.js
+++ Javascript Files/JS Files/Arrays2.js
@@ -67,15 +67,15 @@ console.log(no);
 function bubbleSort(arr) {
   let a = [...arr];
 
-//   for (let i = 0; i < a.length; i++) {
-//     for (let j = 0; j < a.length - i - 1; j++) {
-//       if (a[j] > a[j + 1]) {
-//         [a[j], a[j + 1]] = [a[j + 1], a[j]];
-//       }
-//     }
-//   }
-//   return a;
-// }
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a.length - i - 1; j++) {
+      if (a[j] > a[j + 1]) {
+        [a[j], a[j + 1]] = [a[j + 1], a[j]];
+      }
+    }
+  }
+  return a;
+}
 
 //console.log(bubbleSort([5, 3, 1, 4]));
 
