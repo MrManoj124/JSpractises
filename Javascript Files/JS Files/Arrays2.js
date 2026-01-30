b3213e4341510c3c626fb1426c03aeecb85ddb1a
MrManoj124 <manojmanorooban@gmail.com>
2026-01-30 22:32:18 +0530

    create binarysearch function

--- Javascript Files/JS Files/Arrays2.js
+++ Javascript Files/JS Files/Arrays2.js
@@ -13,3 +13,18 @@ console.log(linearSearch(nums, 30)); // 2
 
 
 //Binary Search (Sorted Arrays Only)
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+
+  return -1;
+}
+
