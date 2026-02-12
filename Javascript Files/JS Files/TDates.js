a5ea8a467de3bff6e55bbf28aa6c114564ca0956
MrManoj124 <manojmanorooban@gmail.com>
2026-02-13 00:04:41 +0530

    check the condition with today's date

--- Javascript Files/JS Files/TDates.js
+++ Javascript Files/JS Files/TDates.js
@@ -62,8 +62,8 @@ const today = new Date();
 const someday = new Date();
 someday.setFullYear(2100, 0, 14);
 
-// if (someday > today) {
-//   text = "Today is before January 14, 2100.";
-// } else {
-//   text = "Today is after January 14, 2100.";
-// }
+if (someday > today) {
+  text = "Today is before January 14, 2100.";
+} else {
+  text = "Today is after January 14, 2100.";
+}
