b16acb74ed1a111d389b4d036dc2a5cab8a5f969
MrManoj124 <manojmanorooban@gmail.com>
2026-02-13 00:14:16 +0530

    print temporal year

--- Javascript Files/JS Files/TDates.js
+++ Javascript Files/JS Files/TDates.js
@@ -83,3 +83,4 @@ console.log(`Difference in years: ${diffDays / 365}`);
 const { Temporal } = require('@js-temporal/polyfill');
 const temporalDate = Temporal.PlainDate.from('2024-01-01');
 console.log(`Temporal date: ${temporalDate.toString()}`);
+console.log(`Year: ${temporalDate.year}, Month: ${temporalDate.month}, Day: ${temporalDate.day}`);
