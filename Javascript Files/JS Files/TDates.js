8c872b7404ea04525752faf245262f8466e6ca23
MrManoj124 <manojmanorooban@gmail.com>
2026-02-12 23:41:28 +0530

    use Date formats

--- Javascript Files/JS Files/TDates.js
+++ Javascript Files/JS Files/TDates.js
@@ -9,3 +9,6 @@ const date2 = new Date('2024-12-31');
 const timeDiff = date2.getTime() - date1.getTime();
 const daysDiff = timeDiff / (1000 * 3600 * 24);
 console.log(`Number of days between ${date1.toDateString()} and ${date2.toDateString()}: ${daysDiff}`);
+
+//Date Formates
+const date = new Date();
