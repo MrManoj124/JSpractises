dc9e729749b61d6f05f5abcf442161999f23f26a
MrManoj124 <manojmanorooban@gmail.com>
2026-02-12 23:16:23 +0530

    Print the numbers between the date

--- Javascript Files/JS Files/TDates.js
+++ Javascript Files/JS Files/TDates.js
@@ -8,3 +8,4 @@ const date1 = new Date('2024-01-01');
 const date2 = new Date('2024-12-31');
 const timeDiff = date2.getTime() - date1.getTime();
 const daysDiff = timeDiff / (1000 * 3600 * 24);
+console.log(`Number of days between ${date1.toDateString()} and ${date2.toDateString()}: ${daysDiff}`);
