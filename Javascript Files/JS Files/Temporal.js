15f94037eade5a1bbd54c891d615f051a2c8da03
MrManoj124 <manojmanorooban@gmail.com>
2026-02-27 23:00:51 +0530

    Create a today note

--- Javascript Files/JS Files/Temporal.js
+++ Javascript Files/JS Files/Temporal.js
@@ -1,3 +1,5 @@
 let d = new Date(2026, 1, 27); //1 = Febrauary
 
 let da = new Date(); d.setDate(d.getDate() + 7); // Changes the original object 
+
+let today = Temporal.Now.plainDateISO(); let nextWeek = today.add({ days: 7 });
