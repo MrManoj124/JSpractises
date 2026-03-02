dda2a363298fd68751957ac0752e3e9e91ae10d9
MrManoj124 <manojmanorooban@gmail.com>
2026-03-02 23:25:48 +0530

    Get the Current Date and Time

--- Javascript Files/JS Files/Temporal2.js
+++ Javascript Files/JS Files/Temporal2.js
@@ -1,2 +1,6 @@
 //<--Create a PlainDateTime->
 const dateTime = Temporal.PlainDateTime.from("2026-05-17T10:00:00");
+
+
+//<--Get the Current Date and Time-->
+const now = Temporal.Now.plainDateTimeISO();
