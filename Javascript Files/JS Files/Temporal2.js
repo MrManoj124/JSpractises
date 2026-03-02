4f084e550612cbc533f67ad8486d336169697282
MrManoj124 <manojmanorooban@gmail.com>
2026-03-02 23:27:37 +0530

    Add or Subtract Time

--- Javascript Files/JS Files/Temporal2.js
+++ Javascript Files/JS Files/Temporal2.js
@@ -4,3 +4,10 @@ const dateTime = Temporal.PlainDateTime.from("2026-05-17T10:00:00");
 
 //<--Get the Current Date and Time-->
 const now = Temporal.Now.plainDateTimeISO();
+
+
+//<--Add or Subtract Time-->
+const dateTimes = Temporal.PlainDateTime.from("2026-05-17T10:00:00");
+
+const earlier = dateTimes.subtract({ minutes: 30 });
+const later = dateTimes.add({ hours: 2 });
