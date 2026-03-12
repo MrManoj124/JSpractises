471465282b0f07454b9180144ced62efb493175d
MrManoj124 <manojmanorooban@gmail.com>
2026-03-12 15:34:04 +0530

    define Main Temporal Objects

---
+++ Javascript Files/JS Files/Temp_Reference.js
@@ -0,0 +1,9 @@
+// Main Temporal Objects
+
+// Temporal objects are the core part of the Temporal API which aims to replace the old Date object.
+
+// All Temporal objects are immutable, which helps prevent bugs related to accidental modification of time values.
+// Object	Description
+// Temporal.Now	The current time
+// Temporal.ZonedDateTime	Date and time in a specific time zone
+// // Temporal.Instant	A fixed point in time, independent of time zone
