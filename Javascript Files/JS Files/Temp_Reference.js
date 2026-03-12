690fafccc170aaad183858cfa0a6754b83c6e1d2
MrManoj124 <manojmanorooban@gmail.com>
2026-03-12 15:37:59 +0530

    Define Temporal.ZonedDateTime

--- Javascript Files/JS Files/Temp_Reference.js
+++ Javascript Files/JS Files/Temp_Reference.js
@@ -25,3 +25,23 @@ Temporal.PlainMonthDay()	Month and day only (06-12)*/
 
 // Example
 const now = Temporal.Now.zonedDateTimeISO();
+
+
+//<===Temporal.ZonedDateTime===>
+
+// A Temporal.ZonedDateTime is a timezone and calendar-aware date/time object that represents a real time event from the perspective of a particular region on Earth.
+// Example: August 15th, 1984 at 3:24 AM in US Pacific time (in Gregorian calendar).
+
+Example
+const zonedDate = Temporal.ZonedDateTime.from({
+  timeZone: 'America/Los_Angeles',
+  year: 1995,
+  month: 12,
+  day: 7,
+  hour: 3,
+  minute: 24,
+  second: 30,
+  millisecond: 0,
+  microsecond: 3,
+  nanosecond: 500
+});
