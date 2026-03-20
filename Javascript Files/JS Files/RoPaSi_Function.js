b53f1acf23ae4013fa40050ff3e055427141dbf5
MrManoj124 <manojmanorooban@gmail.com>
2026-03-20 22:01:16 +0530

    create a Game flow function

--- Javascript Files/JS Files/RoPaSi_Function.js
+++ Javascript Files/JS Files/RoPaSi_Function.js
@@ -1,4 +1,10 @@
 //Rock, paper, Scissors Refactored with Functions
 const InitialGame = () =>{
+    const startGame = confirm("Shall we play rock, paper or scissor?");
+    startGame ? playGame() : alert("Ok, Maybe next time.");
+};
+
+//Game flow function
+const playGame = () =>{
     
 }
