f08e7d4153280b18644ed9b3306b08e7af995625
MrManoj124 <manojmanorooban@gmail.com>
2026-03-23 23:18:29 +0530

    Example 02 for Array ForEach method

--- Javascript Files/JS Files/Array_Iterations.js
+++ Javascript Files/JS Files/Array_Iterations.js
@@ -6,3 +6,12 @@ numbers.forEach(myFunction);
 function myFunction(value, index, array) {
   txt += value + "<br>";
 }
+
+//Example 02 for Array ForEach method
+const numbers = [45, 4, 9, 16, 25];
+let txt = "";
+numbers.forEach(myFunction);
+
+function myFunction(value) {
+  txt += value + "<br>";
+}
