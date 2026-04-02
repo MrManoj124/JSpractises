2fe6ebac0a65927ee2e0f0b28b2ff45c0e8f5892
MrManoj124 <manojmanorooban@gmail.com>
2026-04-02 23:29:35 +0530

    create numberss Array

--- Javascript Files/JS Files/Array_Iterations.js
+++ Javascript Files/JS Files/Array_Iterations.js
@@ -24,3 +24,9 @@ const numbers2 = numbers1.map(myFunction);
 function myFunction01(value, index, array) {
   return value * 2;
 }
+
+//Array forEach() method does not return a new array, it simply executes the provided function once for each array element.
+const numberss = [45,14,25,63,87,95];
+let text = "";
+numberss.forEach(myFunction);
+
