197a0305ec55d67b469777554d0d7600962a356f
MrManoj124 <manojmanorooban@gmail.com>
2026-05-17 15:50:17 +0530

    create a function as insertionSort

--- Javascript Files/JS Files/Sorting_Test.js
+++ Javascript Files/JS Files/Sorting_Test.js
@@ -37,3 +37,18 @@ function selectionSort(arr){
     }
     return arr;
 }
+
+// 03. Insertion Sort
+function insertionSort(arr){
+    let n = arr.length;
+    for(let i=1; i<n; i++){
+        let key = arr[i];
+        let j = i-1;
+        while(j >= 0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j = j-1;
+        }
+        arr[j+1] = key;
+    }
+    return arr;
+}
